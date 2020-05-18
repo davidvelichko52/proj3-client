e99abc7dbc7f4ee31c534cf3265333a6bfaf93d0
davidvelichko52 <davidvelichko52@gmail.com>
2020-05-18 11:23:24 -0700

    first

---
+++ src/nav/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      Made by Brandi in 2020
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/nav/Nav.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = props => {
+  const handleLogout = e => {
+    e.preventDefault()
+    // Update the state of the App
+    props.updateToken()
+  }
+
+  let links = (
+    <span>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <Link to="/signup">Signup</Link>
+      </li>
+    </span>
+  )
+
+  // If the user is logged in, show profile page and logout links
+  if (props.user) {
+    links = (
+      <span>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <a href="/" onClick={handleLogout}>Logout</a>
+        </li>
+      </span>
+    )
+  }
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        {links}
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
