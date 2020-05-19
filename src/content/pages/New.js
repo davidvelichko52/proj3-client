8e9128e8caeeb214f760c8db34053cb6fed97f52
samuelhhawkins <samuel.h.hawkins@hotmail.com>
2020-05-19 10:33:31 -0700

    created newpost stub bihhhhh

---
+++ src/content/pages/New.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const New = props => {
+  return (
+    <div>New posts form</div>
+  )
+}
+export default New
--- src/nav/Footer.js
+++ src/nav/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-      Made by Brandi in 2020
+      Made by us in 2020
     </footer>
   )
 }
