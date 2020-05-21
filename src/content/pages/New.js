529520993b0c48e43e01afe0bfadd1cec290de8a
samuelhhawkins <samuel.h.hawkins@hotmail.com>
2020-05-21 11:57:40 -0700

    created upload widget

--- src/content/pages/New.js
+++ src/content/pages/New.js
@@ -7,6 +7,55 @@ let [content, setContent] = useState('')
 let [caption, setCaption] = useState('')
 let [isSubmit, setIsSubmit] = useState(false)
 
+function showUploadWidget(e) {
+  e.preventDefault()
+ window.cloudinary.openUploadWidget({
+    cloudName: "samtube",
+    uploadPreset: "default-preset",
+    sources: [
+        "local",
+        "dropbox",
+        "instagram",
+        "facebook",
+        "camera"
+    ],
+    googleApiKey: "<image_search_google_api_key>",
+    showAdvancedOptions: true,
+    cropping: false,
+    multiple: true,
+    defaultSource: "local",
+    styles: {
+        palette: {
+            window: "#000000",
+            sourceBg: "#000000",
+            windowBorder: "#8E9FBF",
+            tabIcon: "#FFFFFF",
+            inactiveTabIcon: "#8E9FBF",
+            menuIcons: "#2AD9FF",
+            link: "#08C0FF",
+            action: "#336BFF",
+            inProgress: "#00BFFF",
+            complete: "#33ff00",
+            error: "#EA2727",
+            textDark: "#000000",
+            textLight: "#FFFFFF"
+        },
+        fonts: {
+            default: null,
+            "'Space Mono', monospace": {
+                url: "https://fonts.googleapis.com/css?family=Space+Mono",
+                active: true
+            }
+        }
+    }
+},
+ (err, result) => {
+   if (!err && result && result.event === "success") {
+       console.log('done! Here is the image info: ', result.info);
+       setPic(result.info.secure_url)
+     }
+  });
+ }
 
 
 const handleSubmit = e => {
@@ -26,19 +75,19 @@ const handleSubmit = e => {
           'Authorization': `Bearer ${token}`
         }
       })
-      .then(response =>{ 
+      .then(response =>{
         response.json()
         setIsSubmit(true)
         window.location.reload(false);
-        
+
       }
     )
-      
+
         .catch(err => {
             console.log('ERROR SUBMITTING:', err)
           })
     }
-  
+
     if (isSubmit) {
       return <Redirect to="/profile" />
     }
@@ -50,7 +99,7 @@ const handleSubmit = e => {
       <form onSubmit={handleSubmit}>
         <div>
           <h3>Picture Link:</h3>
-          <input name="pic" placeholder="link to pic" onChange={e => setPic(e.target.value)} />
+          <button onClick={showUploadWidget}>Upload pics</button>
         </div>
         <div>
           <h3>Content:</h3>
@@ -68,5 +117,3 @@ const handleSubmit = e => {
   )
 }
 export default New
-
-
